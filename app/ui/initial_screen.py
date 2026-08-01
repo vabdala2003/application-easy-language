4d04688f919ebb61f4cc7547a818b2afb8154923
vabdala2003 <viniciusmta2003@gmail.com>
2026-08-01 16:35:54 -0300

    icon

--- app/ui/initial_screen.py
+++ app/ui/initial_screen.py
@@ -1,3 +1,4 @@
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import (
     QMainWindow,
     QVBoxLayout,
@@ -13,5 +14,13 @@ class InitialScreen(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.configure_window()
+        self.load_ui()
+
+    def configure_window(self):
         self.setWindowTitle("EasyLanguage")
         self.setMinimumSize(800, 500)
+        self.setWindowIcon(QIcon("app/ui/assets/icons/easy-language-ico.ico"))
+
+    def load_ui(self):
+        pass
